bb40b3679ea1d08c649de78d2b602d725bcdf25d
Amraj Bassi <amraj@rtisan.co.uk>
2016-12-29 19:43:31 +0000

    Initial Add

---
+++ AliceWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AliceWebsite.Startup))]
+namespace AliceWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
